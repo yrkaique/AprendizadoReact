75530cf68fd7f912891347e786229ddbf8a34807
yrkaique <kaiquebjesus22@gmail.com>
2026-09-02 17:15:28 -0300

    Update App.jsx

--- introducao/src/App.jsx
+++ introducao/src/App.jsx
@@ -1,10 +1,26 @@
 
 function App() {
 
+  let nome = "Kaique"
+  let sobrenome = "Jesus"
+  let nome_completo = nome + " " + sobrenome
+
+  let cliques = 0 // let cliques = useState(0) // useState é uma função do React que permite criar um estado para a variável cliques
+
+  function aumentarCliques() {
+    cliques += 1
+  }
+
   return (
     <div>
-      <h1>Olá Mundo!</h1>
-      <p>Estou aprendendo React!</p>
+      <h1>Olá {2+2} Mundo!</h1>
+      <p>Estou apren{console.log("flores")}dendo React!</p>
+      <p>Meu nome é {nome_completo}</p>
+
+      <hr/>
+
+      <p>Você clicou {cliques} vezes</p>
+      <button onClick={aumentarCliques}>Clique aqui!</button>
     </div>
   )
 }
